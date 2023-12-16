ce5ac40451033337c8d5c96421665b0eb5fc2da1
Erik Bjur <erik@bjurengineering.com>
2023-12-15 19:32:56 -0800

    Added a better entry text

--- Application/main.cpp
+++ Application/main.cpp
@@ -2,7 +2,7 @@
 #include "MathLib/Library.h"
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Here we are going to add two numbers 5 and 6 using a static library..." << std::endl;
     int a = 5;
     int b = 6;
     int result = MathLib::Add(a, b);
